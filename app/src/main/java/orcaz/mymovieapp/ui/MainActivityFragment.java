4de64bea4406c37a0589a7fcbe13b7b263250550
OrcaZ <Orca_Z@live.com>
2015-08-27 16:31:03 +0900

    implemented OnMoviesRetrievedListener, show error message if received movie list is null or empty overrode onSaveInstanceState to save movieList as parcelableArrayList in state bundle overrode onCreate so that PosterAdapter would now use movieList from savedInstanceState if exists and only retrieve movies from internet otherwise removed retrieving movie from onCreateOptionMenu check internet connection before kicking off retrieveMoviesTask

--- app/src/main/java/orcaz/mymovieapp/ui/MainActivityFragment.java
+++ app/src/main/java/orcaz/mymovieapp/ui/MainActivityFragment.java
@@ -1,8 +1,7 @@
 package orcaz.mymovieapp.ui;
 
-import android.content.Context;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,13 +11,20 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import orcaz.mymovieapp.R;
+import orcaz.mymovieapp.Util.OnMoviesRetrievedListener;
+import orcaz.mymovieapp.Util.Util;
 import orcaz.mymovieapp.adapter.PosterAdapter;
-import orcaz.mymovieapp.data.Constants;
+import orcaz.mymovieapp.Util.Constants;
+import orcaz.mymovieapp.data.MovieInfo;
 import orcaz.mymovieapp.net.RetrieveMoviesTask;
 
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements OnMoviesRetrievedListener {
     public static final String TAG = MainActivityFragment.class.getSimpleName();
 
     private PosterAdapter mPosterAdapter;
@@ -28,28 +34,42 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPosterAdapter = new PosterAdapter(getActivity());
+        if(savedInstanceState != null){
+            mPosterAdapter.update(savedInstanceState.<MovieInfo>getParcelableArrayList(Constants.MOVIE_DATA));
+        }else{
+            retrieveMovies();
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.poster_recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-        mPosterAdapter = new PosterAdapter(getActivity(), 6);
         recyclerView.setAdapter(mPosterAdapter);
         return view;
+    }
+
+    private void retrieveMovies() {
+        if(Util.IsInternetAvailable(getActivity()))
+            new RetrieveMoviesTask(this).execute(Util.getSortBySetting(getActivity()));
+        else
+            Toast.makeText(getActivity(), "Internet connection not available.", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_main_fragment, menu);
-        int sortBy = getActivity().getPreferences(Context.MODE_PRIVATE).getInt(Constants.SORT_SETTINGS_KEY, Constants.BY_POPULARITY);
-        switch (sortBy){
+        switch (Util.getSortBySetting(getActivity())){
             case Constants.BY_POPULARITY:
                 menu.findItem(R.id.by_popularity).setChecked(true);
-                new RetrieveMoviesTask(mPosterAdapter).execute(RetrieveMoviesTask.BY_POPULARITY);
                 break;
             case Constants.BY_RATING:
                 menu.findItem(R.id.by_rating).setChecked(true);
-                new RetrieveMoviesTask(mPosterAdapter).execute(RetrieveMoviesTask.BY_RATING);
                 break;
         }
     }
@@ -60,18 +80,32 @@ public class MainActivityFragment extends Fragment {
             case R.id.by_popularity:
                 if(!item.isChecked()){
                     item.setChecked(true);
-                    getActivity().getPreferences(Context.MODE_PRIVATE).edit().putInt(Constants.SORT_SETTINGS_KEY, Constants.BY_POPULARITY).commit();
-                    new RetrieveMoviesTask(mPosterAdapter).execute(RetrieveMoviesTask.BY_POPULARITY);
+                    Util.setSortBySetting(getActivity(), Constants.BY_POPULARITY);
+                    retrieveMovies();
                 }
                 break;
             case R.id.by_rating:
                 if(!item.isChecked()){
                     item.setChecked(true);
-                    getActivity().getPreferences(Context.MODE_PRIVATE).edit().putInt(Constants.SORT_SETTINGS_KEY, Constants.BY_RATING).commit();
-                    new RetrieveMoviesTask(mPosterAdapter).execute(RetrieveMoviesTask.BY_RATING);
+                    Util.setSortBySetting(getActivity(), Constants.BY_RATING);
+                    retrieveMovies();
                 }
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(Constants.MOVIE_DATA, new ArrayList<>(mPosterAdapter.getMovieList()));
+    }
+
+    @Override
+    public void onMoviesRetrieved(List<MovieInfo> movieList) {
+        if(movieList != null && movieList.size() > 0)
+            mPosterAdapter.update(movieList);
+        else
+            Toast.makeText(getActivity(), "Error retrieving movie data. Please try again later.", Toast.LENGTH_LONG).show();
     }
 }
