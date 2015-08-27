a398f5b78dc20dfd64ba9b9521da6ffa84fd5631
OrcaZ <Orca_Z@live.com>
2015-08-27 16:28:38 +0900

    added void getMovieList() method for saving instance state in MainActivityFragment deleted showUpdateErrorMessage() method; functionality moved to MainActivityFragment minor tweak using mContext

--- app/src/main/java/orcaz/mymovieapp/adapter/PosterAdapter.java
+++ app/src/main/java/orcaz/mymovieapp/adapter/PosterAdapter.java
@@ -1,14 +1,12 @@
 package orcaz.mymovieapp.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -16,29 +14,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import orcaz.mymovieapp.R;
-import orcaz.mymovieapp.data.Constants;
+import orcaz.mymovieapp.Util.Constants;
 import orcaz.mymovieapp.data.MovieInfo;
 import orcaz.mymovieapp.ui.DetailsActivity;
 
-/**
- * Created by OrcaZ on 2015/08/26.
- */
 public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PosterViewHolder> {
     public static final String TAG = PosterAdapter.class.getSimpleName();
 
     private Context mContext;
-    private int mNum;
     private List<MovieInfo> mMovieList;
 
-    public PosterAdapter(Context context, int num){
+    public PosterAdapter(Context context){
         mContext = context;
-        mNum = num;
         mMovieList = new ArrayList<>();
     }
 
     @Override
     public PosterAdapter.PosterViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = ((Activity) mContext).getLayoutInflater().inflate(R.layout.item_view_poster, viewGroup, false);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_view_poster, viewGroup, false);
         return new PosterViewHolder(view);
     }
 
@@ -47,14 +40,14 @@ public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PosterView
         viewHolder.bindViewHolder(i);
     }
 
+    public List<MovieInfo> getMovieList(){
+        return mMovieList;
+    }
+
     public void update(List<MovieInfo> movies){
         mMovieList.clear();
         mMovieList.addAll(movies);
         notifyDataSetChanged();
-    }
-
-    public void showUpdateErrorMessage(){
-        Toast.makeText(mContext, "Error acquiring movie data, please try again later.", Toast.LENGTH_LONG).show();
     }
 
     @Override
