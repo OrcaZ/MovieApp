014aea63534dd1b7afff26bc4a9d709dc71fe946
OrcaZ <OrcaZ@users.noreply.github.com>
2015-11-13 22:25:48 +0900

    Delete OnMoviesRetrievedListener.java

--- app/src/main/java/orcaz/mymovieapp/Util/OnMoviesRetrievedListener.java
+++
@@ -1,9 +0,0 @@
-package orcaz.mymovieapp.Util;
-
-import java.util.List;
-
-import orcaz.mymovieapp.data.MovieInfo;
-
-public interface OnMoviesRetrievedListener {
-    void onMoviesRetrieved(List<MovieInfo> movieList);
-}
